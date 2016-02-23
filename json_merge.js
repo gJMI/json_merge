021c9918ed3959707870097d25cfeb18b5309a24
Jan Mikes <jmikes014@me.com>
2016-02-23 20:45:48 +0100

    Small test update

--- json_merge.js
+++ json_merge.js
@@ -1,7 +1,7 @@
 var fs=require('fs');
 var extend=require('extend');
 
-console.log(process.argv.length);
+//console.log(process.argv.length);
 
 if (process.argv.length != 5)
 {
@@ -13,11 +13,11 @@ if (process.argv.length != 5)
 var object1 = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 var object2 = JSON.parse(fs.readFileSync(process.argv[3], 'utf8'));
 
-console.log(object1);
-console.log(object2);
+//console.log(object1);
+//console.log(object2);
 
 var result = extend(object1,object2);
 
-console.log(result);
+//console.log(result);
 
 fs.writeFileSync(process.argv[4],JSON.stringify(result,null,2),'utf8');
